1337dea2a0367b84a8a7baef3c830b5f74dd28b5
Thomas McNish <mcnishthomas@gmail.com>
2020-11-27 11:20:10 -0500

    adding inRange method

--- _.js
+++ _.js
@@ -8,6 +8,29 @@ const _ = {
       return currentVal;
     }
   },
+
+  inRange(currentVal, start, end) {
+    if (typeof end === "undefined") {
+    }
+
+    if (currentVal === start) {
+      return true;
+    }
+
+    if (currentVal == end) {
+      return false;
+    }
+
+    if (typeof end !== "undefined") {
+      if (currentVal < start) {
+        return false;
+      } else if (currentVal > end) {
+        return false;
+      } else if (currentVal > start && currentVal < end) {
+        return true;
+      }
+    }
+  },
 };
 
 // Do not write or modify code below this line.
