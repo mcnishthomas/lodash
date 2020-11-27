202d48f6efb1755ad5c234a390d595c4d7928812
Thomas McNish <mcnishthomas@gmail.com>
2020-11-27 09:38:07 -0500

    Added clamp method

--- _.js
+++ _.js
@@ -1,7 +1,14 @@
-
-
-
-
+const _ = {
+  clamp(currentVal, lowerBound, upperBound) {
+    if (currentVal < lowerBound) {
+      return lowerBound;
+    } else if (currentVal > upperBound) {
+      return upperBound;
+    } else if (currentVal > lowerBound && currentVal < upperBound) {
+      return currentVal;
+    }
+  },
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
