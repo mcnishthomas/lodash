817309c4189f322fbda2f471fc36b01fdae3b53f
Thomas McNish <mcnishthomas@gmail.com>
2020-11-28 12:07:38 -0500

    Added findKey method

--- _.js
+++ _.js
@@ -80,6 +80,18 @@ const _ = {
     }
 
     return invertedObject;
+  },
+
+  findKey(object, predicate) {
+    for (let key in object) {
+      let value = object[key];
+      let predicateReturnValue = predicate(value);
+      if (predicateReturnValue) {
+        return key;
+      };
+    };
+    undefined;
+    return undefined;
   }
 };
 
